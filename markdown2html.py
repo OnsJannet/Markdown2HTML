912c10436a9888b34fbae89d4a52c7fd3950ade3
onsjannet <onsjannet@gmail.com>
2021-05-02 13:28:16 +0100

    pep8

--- markdown2html.py
+++ markdown2html.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-import os, fnmatch
 from os import path
 
 '''
@@ -13,10 +12,10 @@ exit(0) if it runs
 
 
 if len(sys.argv) < 2:
-	sys.stderr.write("Usage: ./markdown2html.py README.md README.html \n")
-	exit (1)
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html \n")
+    exit(1)
 elif not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
-	sys.stderr.write("Missing" + " " + sys.argv[1] + "\n")
-	exit (1)
+    sys.stderr.write("Missing" + " " + sys.argv[1] + "\n")
+    exit(1)
 else:
-	exit(0)
+    exit(0)
