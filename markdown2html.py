ef152b7669b312cc4c3e19b144557e49af349f9b
onsjannet <onsjannet@gmail.com>
2021-05-02 15:24:34 +0100

    pep8 correction

--- markdown2html.py
+++ markdown2html.py
@@ -12,17 +12,16 @@ exit(0) if it runs
 '''
 if __name__ == "__main__":
 
-	if len(sys.argv) < 2:
-		sys.stderr.write("Usage: ./markdown2html.py README.md README.html \n")
-		exit(1)
-	elif not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
-		sys.stderr.write("Missing" + " " + sys.argv[1] + "\n")
-		exit(1)	
-	else:
-		with open(sys.argv[1], 'r') as file:
-			text = file.read()
-			html = markdown.markdown(text)
-		with open(sys.argv[2], 'w') as file:
-			file.write(html)
-			exit(0)
-
+    if len(sys.argv) < 2:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html \n")
+        exit(1)
+    elif not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
+        sys.stderr.write("Missing" + " " + sys.argv[1] + "\n")
+        exit(1)
+    else:
+        with open(sys.argv[1], 'r') as file:
+            text = file.read()
+            html = markdown.markdown(text)
+        with open(sys.argv[2], 'w') as file:
+            file.write(html)
+            exit(0)
