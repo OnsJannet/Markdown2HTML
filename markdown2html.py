caa6f930baf7a6eeb47f6495d58347744175532f
onsjannet <onsjannet@gmail.com>
2021-05-03 20:57:57 +0100

    markdown

--- markdown2html.py
+++ markdown2html.py
@@ -7,12 +7,14 @@ from os import path
 
 
 if __name__ == "__main__":
-    # Check for Usage
-    if len(argv) != 3:
-        eprint("Usage: ./markdown2html.py README.md README.html")
-        exit(1)
-    elif not path.exists(argv[1]):
-        eprint("Missing {}".format(argv[1]))
-        exit(1)
+    """
+    a function that turns markdown to html 
+    """
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
+    elif not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
+        sys.stderr.write("Missing " + sys.argv[1] + "\n")
+        sys.exit(1)
     else:
         sys.exit(0)
