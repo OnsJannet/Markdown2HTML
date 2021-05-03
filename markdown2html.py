1c71a33808d2bd722226faede7f281ce7d3818f2
onsjannet <onsjannet@gmail.com>
2021-05-03 20:50:10 +0100

    markdown

--- markdown2html.py
+++ markdown2html.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     """
     if len(sys.argv) != 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html \n")
-        sys.exit(1)
+        exit(1)
     elif not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
-        sys.stderr.write("Missing " + sys.argv[1] + "\n")
-        sys.exit(1)
+        sys.stderr.write("Missing {}".format(argv[1]))
+        exit(1)
     else:
-        sys.exit(0)
+        exit(0)
